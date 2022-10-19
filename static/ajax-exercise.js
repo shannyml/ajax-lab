9f75f3b206891f34210f4b8cff2ed2c96b08cee9
shannen yap <shannen.yml@gmail.com>
2022-10-19 10:05:14 -0700

    Completed Fortune AJAX and Weather AJAX

--- static/ajax-exercise.js
+++ static/ajax-exercise.js
@@ -1,9 +1,15 @@
 'use strict';
 
 // PART 1: SHOW A FORTUNE
+function replaceFortune(results) {
+  document.querySelector("#fortune-text").innerHTML = results;
+}
 
 function showFortune(evt) {
-  // TODO: get the fortune and show it in the #fortune-text div
+  fetch('/fortune')
+    .then((response) => response.text())
+    .then(replaceFortune);
+  
 }
 
 document.querySelector('#get-fortune-button').addEventListener('click', showFortune);
@@ -15,18 +21,48 @@ function showWeather(evt) {
 
   const url = '/weather.json';
   const zipcode = document.querySelector('#zipcode-field').value;
+  
+  fetch(`/weather.json?zipcode=${zipcode}`)
+    .then((response) => response.json())
+    .then((jsonData) => {
+      document.querySelector('#weather-info').innerHTML = jsonData.forecast;
+  });
 
-  // TODO: request weather with that URL and show the forecast in #weather-info
+
 }
 
 document.querySelector('#weather-form').addEventListener('submit', showWeather);
 
 // PART 3: ORDER MELONS
+function updateMelons() {
+  if (results.code === 'OK') 
+    document.quearySelector('#order-status').innerHTML = <p>${results.msg}</p>
+
+  else
+
+}
 
 function orderMelons(evt) {
   evt.preventDefault();
 
+  const formInputs = {
+    melon: document.querySelector('#melon-type-field').value,
+    qty: document.querySelector('#qty-field').value,
+  };
+  
+  fetch('/order-melons.json', params), {
+    .then((response) => response.json())
+    .then((updateMelons);
+  }
+  const params = {
+    method: 'POST',
+    body: JSON.stringify(formInputs), 
+    headers: {
+      'Content-Type': 'application/json'
+    },
+  }
+  }
   // TODO: show the result message after your form
   // TODO: if the result code is ERROR, make it show up in red (see our CSS!)
-}
+
 document.querySelector('#order-form').addEventListener('submit', orderMelons);
